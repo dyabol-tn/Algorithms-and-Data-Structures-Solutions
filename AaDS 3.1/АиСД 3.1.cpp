69961c9aceafa916836205f6cb52a5b657a540c5
Maks Stepanenko <stepanenkom156@gmail.com>
2025-11-13 09:59:42 +0300

    Add files via upload

---
+++ AaDS 3.1/АиСД 3.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ifstream filein("input.txt");
+	ofstream fileout("output.txt");
+	int size;
+	filein >> size;
+	vector<int> arr(size + 1);
+	bool check = true;
+	for (int i = 1; i <= size; i++) {
+		filein >> arr[i];
+	}
+	for (int i = 1; i <= size / 2; i++) {
+		if ((2 * i <= size && arr[i] > arr[2 * i]) || (2 * i + 1 <= size && arr[i] > arr[2 * i + 1])) {
+			check = false;
+			break;
+		}
+	}
+	if (check) fileout << "Yes";
+	else fileout << "No";
+	filein.close();
+	fileout.close();
+	return 0;
+}
