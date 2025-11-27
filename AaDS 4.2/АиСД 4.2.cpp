c705a8357efd43d67a2017fcff64077998145867
Maks Stepanenko <stepanenkom156@gmail.com>
2025-11-27 09:17:49 +0300

    Add files via upload

--- AaDS 4.2/АиСД 4.2.cpp
+++ AaDS 4.2/АиСД 4.2.cpp
@@ -1,1 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main() {
+	ifstream filein("input.txt");
+	ofstream fileout("output.txt");
+	int size;
+	filein >> size;
+	int* graf = new int[size + 1];
+	for (int i = 0; i <= size; i++) {
+		graf[i] = 0;
+	}
+	int first, second;
+	while (filein >> first >> second) {
+		graf[second] = first;
+	}
+	for (int i = 1; i <= size; i++) {
+		fileout << graf[i] << " ";
+	}
+	filein.close();
+	fileout.close();
+	delete[] graf;
+	return 0;
+}
