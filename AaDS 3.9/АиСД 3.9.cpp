9b7eb77ff27aa1635455d69d17b87b4612a4573b
Maks Stepanenko <stepanenkom156@gmail.com>
2025-11-27 09:24:00 +0300

    Add files via upload

--- AaDS 3.9/АиСД 3.9.cpp
+++ AaDS 3.9/АиСД 3.9.cpp
@@ -1,1 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stack>
 
+using namespace std;
+
+int main() {
+    ifstream filein("input.txt");
+    ofstream fileout("output.txt");
+    int count;
+    filein >> count;
+    if (count % 2 == 0) {
+        stack<int> shlangi;
+        for (int i = 0; i < count; i++) {
+            double x, y;
+            int upShlang;
+            filein >> x >> y >> upShlang;
+            if (!shlangi.empty() && shlangi.top() == upShlang) {
+                shlangi.pop();
+            }
+            else {
+                shlangi.push(upShlang);
+            }
+        }
+        if (shlangi.empty()) {
+            fileout << "Yes";
+        }
+        else {
+            fileout << "No";
+        }
+    }
+    else {
+        fileout << "No";
+    }
+    filein.close();
+    fileout.close();
+    return 0;
+}
