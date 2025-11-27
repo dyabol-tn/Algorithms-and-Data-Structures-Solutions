858b6e286f355c3023354d80ae051d95f727ddf0
Maks Stepanenko <stepanenkom156@gmail.com>
2025-11-27 09:20:35 +0300

    Add files via upload

--- AaDS 4.4/АиСД 4.4.cpp
+++ AaDS 4.4/АиСД 4.4.cpp
@@ -1,1 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+	ifstream filein("input.txt");
+	ofstream fileout("output.txt");
+	int size;
+	filein >> size;
+	bool** matrix = new bool* [size + 1];
+	for (int i = 0; i <= size; i++) {
+		matrix[i] = new bool[size + 1];
+	}
+	for (int i = 1; i <= size; i++) {
+		for (int j = 1; j <= size; j++) {
+			int temp;
+			filein >> temp;
+			matrix[i][j] = temp;
+		}
+	}
+	vector<int> kanon(size + 1, 0);
+	for (int i = 1; i <= size; i++) {
+		for (int j = 1; j <= size; j++) {
+			if (matrix[i][j] == 1) {
+				kanon[j] = i;
+			}
+		}
+	}
+	for (int i = 1; i <= size; i++) {
+		fileout << kanon[i] << " ";
+	}
+	for (int i = 0; i <= size; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+	filein.close();
+	fileout.close();
+	return 0;
+}
