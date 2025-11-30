599c666aede5b555d6fe74794576b96bcd48011a
Maks Stepanenko <stepanenkom156@gmail.com>
2025-12-01 01:10:31 +0300

    Add files via upload

---
+++ AaDS 4.5/АиСД 4.5.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ifstream filein("input.txt");
+	ofstream fileout("output.txt");
+	int vershCount;
+	filein >> vershCount;
+	int** matrix = new int* [vershCount + 1];
+	for (int i = 0; i <= vershCount; i++) {
+		matrix[i] = new int[vershCount + 1];
+	}
+	for (int i = 1; i <= vershCount; i++) {
+		for (int j = 1; j <= vershCount; j++) {
+			filein >> matrix[i][j];
+		}
+	}
+	vector<bool> visited(vershCount + 1, false);
+	vector<int> metki(vershCount + 1, -1);
+	queue<int> q;
+	int metkaNow = 1;
+	for (int i = 1; i <= vershCount; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			metki[i] = metkaNow;
+			metkaNow++;
+			q.push(i);
+		}
+		while (!q.empty()) {
+			int temp = q.front();
+			q.pop();
+			for (int j = 1; j <= vershCount; j++) {
+				if (matrix[temp][j] == 1 && !visited[j]) {
+					visited[j] = true;
+					metki[j] = metkaNow;
+					metkaNow++;
+					q.push(j);
+				}
+			}
+		}
+	}
+	for (int i = 1; i <= vershCount; i++) {
+		fileout << metki[i] << " ";
+	}
+	for (int i = 0; i <= vershCount; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+	filein.close();
+	fileout.close();
+	return 0;
+}
