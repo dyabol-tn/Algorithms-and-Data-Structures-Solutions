1d59f015b7cd281b9475ffd84e6f5a7e2b7ec3a8
Maks Stepanenko <stepanenkom156@gmail.com>
2025-12-01 22:38:50 +0300

    Add files via upload

--- AaDS 3.7/АиСД 3.7.cpp
+++ AaDS 3.7/АиСД 3.7.cpp
@@ -1,1 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+	ifstream filein("input.txt");
+	ofstream fileout("output.txt");
+	int tableSize, keysCount, constC;
+	filein >> tableSize >> constC >> keysCount;
+	vector<int> keys(tableSize, -1);
+	for (int i = 0; i < keysCount; i++) {
+		int key; 
+		filein >> key;
+		bool find = false;
+		for (int i = 0; i < tableSize; i++) {
+			int h = ((key % tableSize) + constC * i) % tableSize;
+			if (keys[h] == key) {
+				find = true;
+				break;
+			}
+		}
+		if (find) continue;
+		for (int i = 0; i < tableSize; i++) {
+			int h = ((key % tableSize) + constC * i) % tableSize;
+			if (keys[h] == -1) {
+				keys[h] = key;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < tableSize; i++) {
+		fileout << keys[i] << " ";
+	}
+	filein.close();
+	fileout.close();
+	return 0;
+}
